e4e28500a6c3e9a5f4c84d5fd549c611be71c3f9
Giedrius Stabingis <gstabingis@gmail.com>
2021-10-18 16:07:15 +0300

    Pertvarkytos straipsnio redagavimo ir sukūrimo formos

--- resources/views/straipsniai/create.blade.php
+++ resources/views/straipsniai/create.blade.php
@@ -6,9 +6,8 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
-
-            <div class="bg-white rounded-lg shadow sm:max-w-xl sm:w-full sm:mx-auto sm:overflow-hidden">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="px-4 py-8 sm:px-10">
                     <div class="relative mt-6">
                         <div class="absolute inset-0 flex items-center">
@@ -24,70 +23,76 @@
                     <div class="mt-6">
                         <form action="{{ route("straipsnis.store") }}" method="POST" class="w-full space-y-6">
                             @csrf
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Pavadinimas
+
+                            <div class="flex mb-4 -mx-2 ">
+                                <div class="w-3/4 px-2 h-screen">
+                                    <div class="w-full h-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Tekstas
+                                        </div>
+                                        <div class=" relative h-full">
+                                            <textarea name="tekstas" required class="h-full rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"></textarea>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class=" relative ">
-                                    <input type="text" name="pavadinimas" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Pavadinimas"/>
+                                <div class="w-1/4 px-2 space-y-6">
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Pavadinimas
+                                        </div>
+                                        <div class=" relative ">
+                                            <input type="text" name="pavadinimas" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Pavadinimas"/>
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Paveiksliukas
+                                        </div>
+                                        <div class=" relative ">
+                                            <input type="text" name="paveiksliukas" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Paveiksliukas"/>
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Aprašymas
+                                        </div>
+                                        <div class=" relative ">
+                                            <textarea name="aprasymas" class="h-48 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Rubrika
+                                        </div>
+                                        <div class=" relative ">
+                                            <select name="rid" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                                                @foreach ($rubrikos as $rubrika)
+                                                    <option value="{{ $rubrika->id }}">{{ $rubrika->pavadinimas }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Nuoroda
+                                        </div>
+                                        <div class=" relative ">
+                                            <input type="text" name="nuoroda" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Nuoroda"/>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <span class="block w-full rounded-md shadow-sm">
+                                            <button type="submit" class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                                Pridėti
+                                            </button>
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Paveiksliukas
-                                </div>
-                                <div class=" relative ">
-                                    <input type="text" name="paveiksliukas" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Paveiksliukas"/>
-                                </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Aprašymas
-                                </div>
-                                <div class=" relative ">
-                                    <textarea name="aprasymas" class="h-48 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"></textarea>
-                                </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Rubrika
-                                </div>
-                                <div class=" relative ">
-                                    <select name="rid" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-                                        @foreach ($rubrikos as $rubrika)
-                                            <option value="{{ $rubrika->id }}">{{ $rubrika->pavadinimas }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Nuoroda
-                                </div>
-                                <div class=" relative ">
-                                    <input type="text" name="nuoroda" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Nuoroda"/>
-                                </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Tekstas
-                                </div>
-                                <div class=" relative ">
-                                    <textarea name="tekstas" required class="h-96 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"></textarea>
-                                </div>
-                            </div>
-                            <div>
-                                <span class="block w-full rounded-md shadow-sm">
-                                    <button type="submit" class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                                        Pridėti
-                                    </button>
-                                </span>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </x-app-layout>
--- resources/views/straipsniai/edit.blade.php
+++ resources/views/straipsniai/edit.blade.php
@@ -6,9 +6,8 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
-
-            <div class="bg-white rounded-lg shadow sm:max-w-xl sm:w-full sm:mx-auto sm:overflow-hidden">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="px-4 py-8 sm:px-10">
                     <div class="relative mt-6">
                         <div class="absolute inset-0 flex items-center">
@@ -22,77 +21,83 @@
                         </div>
                     </div>
                     <div class="mt-6">
-                        <form action="{{ route("straipsnis.update", $straipsnis->id) }}" method="POST" class="w-full space-y-6">
+                        <form action="{{ route("straipsnis.update", $straipsnis->id) }}" method="POST" class="w-full">
                             @csrf
                             @method("PUT")
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Pavadinimas
+
+                            <div class="flex mb-4 -mx-2 ">
+                                <div class="w-3/4 px-2 h-screen">
+                                    <div class="w-full h-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Tekstas
+                                        </div>
+                                        <div class=" relative h-full">
+                                            <textarea name="tekstas" required class="h-full rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">{{ $straipsnis->tekstas }}</textarea>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class=" relative ">
-                                    <input type="text" name="pavadinimas" value="{{ $straipsnis->pavadinimas }}" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Pavadinimas"/>
+                                <div class="w-1/4 px-2 space-y-6">
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Pavadinimas
+                                        </div>
+                                        <div class=" relative ">
+                                            <input type="text" name="pavadinimas" value="{{ $straipsnis->pavadinimas }}" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Pavadinimas"/>
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Paveiksliukas
+                                        </div>
+                                        <div class=" relative ">
+                                            <input type="text" name="paveiksliukas" value="{{ $straipsnis->paveiksliukas }}" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Paveiksliukas"/>
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Aprašymas
+                                        </div>
+                                        <div class=" relative ">
+                                            <textarea name="aprasymas" class="h-48 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">{{ $straipsnis->aprasymas }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Rubrika
+                                        </div>
+                                        <div class=" relative ">
+                                            <select name="rid" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                                                @foreach ($rubrikos as $rubrika)
+                                                    @if ($rubrika->id == $straipsnis->rid)
+                                                        <option value="{{ $rubrika->id }}" selected>{{ $rubrika->pavadinimas }}</option>
+                                                    @else
+                                                        <option value="{{ $rubrika->id }}">{{ $rubrika->pavadinimas }}</option>
+                                                    @endif
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class=" relative text-gray-600 text-sm">
+                                            Nuoroda
+                                        </div>
+                                        <div class=" relative ">
+                                            <input type="text" name="nuoroda" value="{{ $straipsnis->nuoroda }}" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Nuoroda"/>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <span class="block w-full rounded-md shadow-sm">
+                                            <button type="submit" class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                                Keisti
+                                            </button>
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Paveiksliukas
-                                </div>
-                                <div class=" relative ">
-                                    <input type="text" name="paveiksliukas" value="{{ $straipsnis->paveiksliukas }}" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Paveiksliukas"/>
-                                </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Aprašymas
-                                </div>
-                                <div class=" relative ">
-                                    <textarea name="aprasymas" class="h-48 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">{{ $straipsnis->aprasymas }}</textarea>
-                                </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Rubrika
-                                </div>
-                                <div class=" relative ">
-                                    <select name="rid" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-                                        @foreach ($rubrikos as $rubrika)
-                                            @if ($rubrika->id == $straipsnis->rid)
-                                                <option value="{{ $rubrika->id }}" selected>{{ $rubrika->pavadinimas }}</option>
-                                            @else
-                                                <option value="{{ $rubrika->id }}">{{ $rubrika->pavadinimas }}</option>
-                                            @endif
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Nuoroda
-                                </div>
-                                <div class=" relative ">
-                                    <input type="text" name="nuoroda" value="{{ $straipsnis->nuoroda }}" required class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Nuoroda"/>
-                                </div>
-                            </div>
-                            <div class="w-full">
-                                <div class=" relative text-gray-600 text-sm">
-                                    Tekstas
-                                </div>
-                                <div class=" relative ">
-                                    <textarea name="tekstas" required class="h-96 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">{{ $straipsnis->aprasymas }}</textarea>
-                                </div>
-                            </div>
-                            <div>
-                                <span class="block w-full rounded-md shadow-sm">
-                                    <button type="submit" class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                                        Keisti
-                                    </button>
-                                </span>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </x-app-layout>
